e8d1ea725556509d0d6704b8915d7730f105ca8b
Aabis-Ahmed-Hassan <aabisahmedhassan3@gmail.com>
2024-02-29 07:32:31 +0500

    firebase integration completed

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        backgroundColor: Colors.red,
-      ),
+      home: Scaffold(),
     );
   }
 }
